64caf6c3784646a5ef2236e4684eea5101275d34
aleksem07 <aleksem07@gmail.com>
2023-12-07 01:35:56 +0300

    feat: add styles loader

--- graphql-app/src/app/loading.tsx
+++ graphql-app/src/app/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
     <main className="flex-1 flex items-center justify-center">
-      <h2>Loading...</h2>
+      <div className="flex justify-center items-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white-900"></div>
+      </div>
     </main>
   );
 }
