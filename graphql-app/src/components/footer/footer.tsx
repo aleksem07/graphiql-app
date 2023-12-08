14838db01addedd06427007174a1dc7ca91bac72
aleksem07 <aleksem07@gmail.com>
2023-12-08 23:22:55 +0300

    feat: add footer links

--- graphql-app/src/components/footer/footer.tsx
+++ graphql-app/src/components/footer/footer.tsx
@@ -1,17 +1,27 @@
 import Link from 'next/link';
+import GitHubIcon from '@/UI/github-icon';
+import RsSchoolIcon from '@/UI/rsschool-icon';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
+    <footer className="bg-gray-800 text-white pt-4 text-center" data-testid="footer">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} All Rights Reserved.</p>
-        <nav className="mt-2">
-          <Link href="#" className="inline-block mx-2 hover:text-gray-300">
-            RS School
+        <nav className="flex justify-center items-center">
+          <Link href="https://github.com/irinaosp" className="inline-block mx-2" target="_blank">
+            <GitHubIcon className="text-white hover:text-gray-500" />
           </Link>
-          <Link href="#" className="inline-block mx-2 hover:text-gray-300">
-            GitHub
+          <Link href="https://github.com/juliaavona" className="inline-block mx-2" target="_blank">
+            <GitHubIcon className="text-white hover:text-gray-500" />
           </Link>
+          <Link href="https://github.com/aleksem07" className="inline-block mx-2" target="_blank">
+            <GitHubIcon className="text-white hover:text-gray-500" />
+          </Link>
+        </nav>
+        <nav className="flex justify-center items-center">
+          <Link href="https://rs.school/react/" className="inline-block mx-2" target="_blank">
+            <RsSchoolIcon className="text-white hover:text-gray-500" />
+          </Link>
+          <p>&copy; 2023 All rights reserved</p>
         </nav>
       </div>
     </footer>
