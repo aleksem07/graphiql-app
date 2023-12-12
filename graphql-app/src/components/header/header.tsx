637d971e64ec479cc46ec5f405104b370ff882db
Irina <108676588+IrinaOsp@users.noreply.github.com>
2023-12-12 22:02:42 +0100

    feat: move header buttons to separate component

--- graphql-app/src/components/header/header.tsx
+++ graphql-app/src/components/header/header.tsx
@@ -1,25 +1,8 @@
-'use client';
-
 import Link from 'next/link';
 import { AppRoutes } from '@/common/routes';
-import { RootState } from '@/redux/store';
-import translation from '@/common/translation';
-import { logout } from '@/firebase';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { removeUser } from '@/redux/user/userSlice';
+import NavLinks from '../navLinks/navLinks';
 
 export default function Header() {
-  const isUserSignedIn: boolean = useAppSelector((state: RootState) => state.userSlice.isSignedIn);
-  const userEmail: string | null = useAppSelector((state: RootState) => state.userSlice.email);
-  const dispatch = useAppDispatch();
-
-  const language = 'en';
-
-  const handleLogout = () => {
-    logout();
-    dispatch(removeUser());
-  };
-
   return (
     <header className="bg-gray-800 text-white p-2" data-testid="header">
       <div className="container mx-auto flex justify-between items-center">
@@ -27,30 +10,7 @@ export default function Header() {
           Logo
         </Link>
         <nav className="space-x-4">
-          {!isUserSignedIn && (
-            <>
-              <Link href={AppRoutes.SING_IN} className="hover:text-gray-300">
-                Sign In
-              </Link>
-              <Link href={AppRoutes.SING_UP} className="hover:text-gray-300">
-                Sign Up
-              </Link>
-            </>
-          )}
-          {isUserSignedIn && (
-            <>
-              <span className="text-xs">
-                {translation.header.loggedInAs[language]}
-                {userEmail}
-              </span>
-              <Link href={AppRoutes.GRAPHQL} className="hover:text-gray-300">
-                {translation.buttons.toGraphqlPage[language]}
-              </Link>
-              <button onClick={handleLogout} className="hover:text-gray-300">
-                {translation.buttons.logout[language]}
-              </button>
-            </>
-          )}
+          <NavLinks />
         </nav>
       </div>
     </header>
---
+++ graphql-app/src/components/navLinks/navLinks.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Link from 'next/link';
+import { AppRoutes } from '@/common/routes';
+import translation from '@/common/translation';
+import { auth, logout } from '@/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect } from 'react';
+
+export default function NavLinks() {
+  const [user, loading] = useAuthState(auth);
+  const userEmail = user?.email;
+  const language = 'en';
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    if (loading) return;
+  }, [loading]);
+
+  return (
+    <>
+      {!userEmail && (
+        <>
+          <Link href={AppRoutes.SING_IN} className="hover:text-gray-300">
+            Sign In
+          </Link>
+          <Link href={AppRoutes.SING_UP} className="hover:text-gray-300">
+            Sign Up
+          </Link>
+        </>
+      )}
+      {userEmail && (
+        <>
+          <span className="text-xs">
+            {translation.header.loggedInAs[language]}
+            {userEmail}
+          </span>
+          <Link href={AppRoutes.GRAPHQL} className="hover:text-gray-300">
+            {translation.buttons.toGraphqlPage[language]}
+          </Link>
+          <button onClick={handleLogout} className="hover:text-gray-300">
+            {translation.buttons.logout[language]}
+          </button>
+        </>
+      )}
+    </>
+  );
+}
