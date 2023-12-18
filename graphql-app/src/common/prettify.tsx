09aecef6eb044c15883890fd6ae759202701a9d5
aleksem07 <aleksem07@gmail.com>
2023-12-19 01:05:57 +0300

    feat: add prettyLine

--- graphql-app/src/common/prettify.tsx
+++ graphql-app/src/common/prettify.tsx
@@ -4,25 +4,62 @@ type PrettifyProps = {
 };
 
 export const Prettify = ({ query, setQuery }: PrettifyProps) => {
-  const handleEditorChange = () => {
-    const prettifyQuery = query
-      .trim()
-      .toLowerCase()
-      .split('')
-      .map((item, index, array) => {
-        if (item === '{' || item === '}') {
-          if (item === '{' && array[index + 1] !== '\n') {
-            return `${item}\n  `;
-          }
-          if (item === '}' && array[index - 1] !== '\n') {
-            return `\n${item}`;
-          }
+  const TAB_SIZE = 2;
+  let indentation = 0;
+
+  const handleIndentation = () => ' '.repeat(TAB_SIZE * indentation);
+
+  const linesArr = query
+    .split('')
+    .map((item, index, array) => {
+      if (item === '{' || item === '}') {
+        if (item === '{' && array[index + 1] !== '\n') {
+          return `${item}\n`;
         }
+        if (item === '}' && array[index - 1] !== '\n') {
+          return `\n${item}`;
+        }
+      }
 
-        return item;
-      })
-      .join('');
+      return item;
+    })
+    .join('')
+    .split('\n');
 
+  const lineWithoutSpace = linesArr.filter((item) => item.trim() !== '');
+
+  const prettyLine = lineWithoutSpace
+    .map((line) => {
+      console.log(line);
+
+      return `${handleIndentation()}${line.trim()}`
+        .split('')
+        .map((item, index, array) => {
+          // console.log(array)
+          if (item === '{') {
+            indentation += 1;
+            if (array[0] === '{') {
+              return `${item.trim()}`;
+            }
+            if (item === '{' && array[index - 1] !== ' ') {
+              return ` ${item}`;
+            }
+          }
+          if (item === '}') {
+            indentation -= 2;
+            indentation < 0 ? (indentation = 0) : indentation;
+          } else {
+          }
+          return item;
+        })
+        .join('');
+    })
+    .join('\n');
+
+  console.log(prettyLine);
+
+  const handleEditorChange = () => {
+    const prettifyQuery = prettyLine;
     setQuery(prettifyQuery);
   };
 
