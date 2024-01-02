ed39cea691fd397370fafadb72afe041953f1e19
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2024-01-01 23:44:34 -0600

    feat: language info save in redux

--- graphql-app/src/components/LangSwitch/LanguageSwitcher.tsx
+++ graphql-app/src/components/LangSwitch/LanguageSwitcher.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setLanguage } from '../../redux/lang/languageSlice';
 import { RootState } from '../../redux/store';
 
 const LangSwitch = () => {
-    const dispatch = useDispatch();
-    const language = useSelector((state: RootState) => state.language.value);
+  const dispatch = useDispatch();
+  const language = useSelector((state: RootState) => state.language.value);
 
-    const toggleLanguage = () => {
-        dispatch(setLanguage(language === 'en' ? 'ru' : 'en'));
-    };
+  const toggleLanguage = () => {
+    dispatch(setLanguage(language === 'en' ? 'ru' : 'en'));
+  };
 
-    return (
-        <div className="flex justify-center my-4">
-            <button
-                onClick={toggleLanguage}
-                className="w-32 px-4 py-2 bg-white hover:bg-gray-100 text-gray-800 font-bold rounded"
-            >
-                {language === 'en' ? 'Русский' : 'English'}
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex justify-center my-4">
+      <button
+        onClick={toggleLanguage}
+        className="w-32 px-4 py-2 bg-white hover:bg-gray-100 text-gray-800 font-bold rounded"
+      >
+        {language === 'en' ? 'Русский' : 'English'}
+      </button>
+    </div>
+  );
 };
 
 export default LangSwitch;
