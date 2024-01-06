e628458f15ec8cdac361e92c102023ee98eaffcd
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-06 17:34:39 +0100

    test: fix developerCard test

--- graphql-app/src/__tests__/pages/welcome/developerCard.test.tsx
+++ graphql-app/src/__tests__/pages/welcome/developerCard.test.tsx
@@ -11,8 +11,8 @@ const mockDeveloper: Developer = {
   githubLink: 'https://github.com/JuliaAvona',
   imageLink: 'https://avatars.githubusercontent.com/u/94717941?v=4',
   description: {
-    en: 'Test Content',
-    ru: 'Тестовый контент',
+    en: ['Test Content'],
+    ru: ['Тестовый контент'],
   },
 };
 
@@ -21,7 +21,7 @@ describe('DeveloperCard', () => {
     render(<DeveloperCard developer={mockDeveloper} lang={'en'} />);
 
     expect(screen.getByText(mockDeveloper.name.en)).toBeInTheDocument();
-    expect(screen.getByText(mockDeveloper.description.en)).toBeInTheDocument();
+    expect(screen.getByText(mockDeveloper.description.en[0])).toBeInTheDocument();
     expect(screen.getByRole('img', { name: mockDeveloper.name.en })).toBeInTheDocument();
     expect(screen.getByRole('link')).toHaveAttribute('href', mockDeveloper.githubLink);
   });
