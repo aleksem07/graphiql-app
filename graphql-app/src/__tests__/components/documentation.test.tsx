a6a83f14ac33dd722a2624981a24967686c1ef79
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-01-07 12:48:37 +0100

    fix: fix doc test

--- graphql-app/src/__tests__/components/documentation.test.tsx
+++ graphql-app/src/__tests__/components/documentation.test.tsx
@@ -7,7 +7,7 @@ describe('Documentation component', () => {
   it('should render text with url request if it is not provided ', () => {
     render(
       <Providers>
-        <Documentation url={''} request={docsRequestEnum.docs} />
+        <Documentation url={''} request={docsRequestEnum.docs} lang="en" />
       </Providers>
     );
     const heading = screen.getByText(/Please select an API/i);
